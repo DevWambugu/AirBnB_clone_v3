454ccd65be749258d5580b5379de11c4a46a99da
DevWambugu <venusdata100@gmail.com>
2023-10-28 04:37:15 +0000

    move storage to main branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -55,7 +56,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +69,25 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        '''retrieves an object'''
+        if cls not in classes.values():
+            return None
+        else:
+            classes_ = models.storage.all(cls)
+            for value in classes_.values():
+                if (value.id == id):
+                    return Value
+            return None
+
+    def count(self, cls=None):
+        '''counts the number of objects in storage'''
+        all_classes = classes.values()
+        if not cls:
+            count = 0
+            for clas in all_classes:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+        return count
