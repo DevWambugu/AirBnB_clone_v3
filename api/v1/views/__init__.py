a0bfbcfee4f5320b3ddcb54936641c5d88af8d71
Onsomujosh <joshuaonsomu@gmail.com>
2023-10-28 14:03:38 -0700

    places1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,8 +10,4 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
-from api.v1.views.places import
-
-if app.config['HBNB_TYPE_STORAGE'] == 'db':
-    from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import *
+from api.v1.views.places import *
