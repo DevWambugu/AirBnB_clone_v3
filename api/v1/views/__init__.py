ffc352220cec9a1adc80696da4c7d1fb0dbc07ec
Onsomujosh <joshuaonsomu@gmail.com>
2023-10-29 12:57:05 -0700

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,6 @@ from flask import Blueprint
 from models import storage
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-app = Flask(__name__)
-app.register_blueprint(app_views)
 
 
 from api.v1.views.index import *
@@ -14,6 +12,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-
-if __name__ == '__main__':
-    app.run(host=host, port=port)
+from api.v1.views.places_reviews import *
